fad5120e02352b5137e9f0208d3a6199695f06b5
agcalvano <agos.yowey@gmail.com>
2016-05-06 21:42:51 -0300

    cambios

--- app/src/main/java/com/example/laboratorioiii/myapplication/MainActivity.java
+++ app/src/main/java/com/example/laboratorioiii/myapplication/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.laboratorioiii.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout recetas;
+    private LinearLayout queCocino;
+    private LinearLayout compartir;
+    private LinearLayout info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    /* public void OnCreate();{
+        recetas = (LinearLayout)findViewById(R.id.linear1);
+        queCocino = (LinearLayout)findViewById(R.id.linear2);
+        compartir = (LinearLayout)findViewById(R.id.linear3);
+        info = (LinearLayout)findViewById(R.id.linear4);*/
+
+
+    }
+
